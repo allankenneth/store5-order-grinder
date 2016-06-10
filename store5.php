28d05d22230b6c94de60df954c255ec445d025ce
allankh <hi@allankenneth.com>
2016-06-09 18:12:41 -0700

    Woohoo! Testing user name change.

--- store5.php
+++ store5.php
@@ -13,6 +13,7 @@ License: GPLv3 or later License
 URI: http://www.gnu.org/licenses/gpl-3.0.html
 WC requires at least: 2.2
 WC tested up to: 2.3
+Woohoo!
 */
 //add_filter('show_admin_bar', '__return_false');
 add_action( 'admin_menu', 'store5_menu' );
